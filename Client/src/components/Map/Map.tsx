16bdbfea201cde367d28fec8c1471815dbda7baf
sbrandon0432 <sbrandon0432@yahoo.com>
2021-02-16 16:28:40 -0800

    Added rout lines

--- Client/src/components/Map/Map.tsx
+++ Client/src/components/Map/Map.tsx
@@ -14,24 +14,52 @@ const Map = () => {
     mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtYnJhbmRvbiIsImEiOiJja2w0Y3M1NGgwZW44MnZqdXhvdjJqanBoIn0.Dh4peFbd4OkT0ljteZ6frA';
     var map = new mapboxgl.Map({
     container: 'root',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-79.4512, 43.6568],
+    zoom: 5,
     });
 
-    map.addControl(
+  map.addControl(
       new MapboxDirections({
           accessToken: mapboxgl.accessToken
       }),
       'top-left'
   );
 
+  map.on('load', function () {
+    map.addSource('route', {
+        'type': 'geojson',
+        'data': {
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+                'type': 'LineString',
+                'coordinates': [
+                    [-111.84779,37.63942],
+                    [-111.84779,37.73942]
+                ]
+            }
+        }
+    });
 
+    map.addLayer({
+        'id': 'route',
+        'type': 'line',
+        'source': 'route',
+        'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        'paint': {
+            'line-color': '#888',
+            'line-width': 8
+        }
+    });
+  });
 
   return (
-    <h1 className="testing">testing</h1>
-  );
-
+    <h1>test</h1>
+  )
 };
 
-
 export default Map;
-
